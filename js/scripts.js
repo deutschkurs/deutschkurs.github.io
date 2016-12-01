f19c28a603c16226f8c0e1b64a85d747a0225818
Davide Lionetti <davidelionettii@gmail.com>
2016-12-01 11:28:01 +0100

    dvfbgdfh

---
+++ js/scripts.js
@@ -0,0 +1,65 @@
+$( document ).ready(function() {
+    setTimeout(function() {
+        $('.preContent').find('.overlay').css('opacity','1');
+        $('header').show();
+    }, 1500);
+});
+
+$('.preContent').superslides({
+    inherit_width_from: '.preContent',
+    inherit_height_from: '.preContent',
+    'play': 0
+});
+
+$(".headline").fitText(1.2, { minFontSize: '30px', maxFontSize: '70px' });
+
+$( ".fa-bars.for-mobile" ).click(function() {
+    
+    $( ".slidingBody, header" ).animate({
+        left: "50%",
+    }, 250);
+    
+    $( ".slidingBody" ).addClass('slided');
+    
+    $(".fa-bars").hover().css('cursor','default');
+
+  setTimeout(function(){
+  }, 200);
+    
+});
+
+$(".closeMenu, .fa-times").click(function() {
+    $( ".slidingBody, header" ).animate({
+        left: "0",
+    }, 250);
+    $('.slidingBody').removeClass('slided');
+    $(".fa-bars").hover().css('cursor','pointer');
+});
+
+$('.spacer, .mobileMenu ul li').click(function() {
+    setTimeout(function(){
+        $( ".slidingBody, header" ).animate({
+            left: "0",
+        }, 250);
+        
+        $('.slidingBody').removeClass('slided');
+    
+        $(".fa-bars").hover().css('cursor','pointer');
+  }, 150);
+});
+
+
+$( ".flexLesson" ).click(function() {
+    $(this).parent().find('.lessonContent').slideToggle("fast", function() {
+    });
+    $(this).parent().toggleClass("expanded");
+    $(this).find('.fa').toggleClass( 'fa-arrow-down fa-arrow-up' );
+});
+
+function pasuser(form) { 
+if (form.id.value=="Emanuele") { 
+if (form.pass.value=="halloberlin") { $('.loginOverlay').fadeOut('slow'); $('html, body').css('overflow-y', 'scroll'); } 
+else { alert("Wrong Password") } 
+} 
+else { alert("Wrong Username") } 
+}
